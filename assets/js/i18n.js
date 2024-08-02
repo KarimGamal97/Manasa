6c2fbc655e12c2d1a30d9990fb5f536ab28b0b3f
Karim_Gamal <81161929+KarimGamal97@users.noreply.github.com>
2024-08-02 05:16:25 +0300

    ssssss

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -17,3 +17,23 @@ const setLanguage = (language) => {
   });
   document.dir = language === "ar" ? "rtl" : "ltr";
 };
+
+const languageSelectorSmallMenu = document.getElementById(
+  "languageSelectorSmallMenu"
+);
+languageSelectorSmallMenu.addEventListener("change", (event) => {
+  setLanguageSmallMenu(event.target.value);
+  localStorage.setItem("lang", event.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  setLanguageSmallMenu(localStorage.getItem("lang"));
+});
+const setLanguageSmallMenu = (language) => {
+  const elements = document.querySelectorAll("[data-i18n]");
+  elements.forEach((element) => {
+    const translationKey = element.getAttribute("data-i18n");
+    element.textContent = translations[language][translationKey];
+  });
+  document.dir = language === "ar" ? "rtl" : "ltr";
+};
